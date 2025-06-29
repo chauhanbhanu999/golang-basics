b2e6e20312e5acae65737ed8c9e89ba389a1bec8
Bhanu Chauhan <54548307+chauhanbhanu999@users.noreply.github.com>
2025-06-30 04:55:45 +0530

    objects + object behaviours

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,11 +1,17 @@
 package bookstore
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type Book struct {
-	Title  string
-	Author string
-	Copies int
+	Title           string
+	Author          string
+	Copies          int
+	ID              int
+	PriceCents      int
+	DiscountPercent int
 }
 
 func Buy(b Book) (Book, error) {
@@ -14,4 +20,26 @@ func Buy(b Book) (Book, error) {
 	}
 	b.Copies--
 	return b, nil
+}
+
+func GetAllBooks(catalog map[int]Book) []Book {
+	result := []Book{}
+	for _, b := range catalog {
+		result = append(result, b)
+	}
+	return result
+}
+
+func GetBook(catalog map[int]Book, ID int) (Book, error) {
+	book, ok := catalog[ID]
+	if !ok {
+		return Book{}, fmt.Errorf("invalid book id:%d", ID)
+	}
+	return book, nil
+}
+
+func (book Book) NetPriceCents() int {
+	saved := book.PriceCents * book.DiscountPercent / 100
+	dicountedPrice := book.PriceCents - saved
+	return dicountedPrice
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -2,7 +2,10 @@ package bookstore_test
 
 import (
 	"bookstore"
+	"sort"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestBook(t *testing.T) {
@@ -42,5 +45,66 @@ func TestNoCopiesLeft(t *testing.T) {
 	_, err := bookstore.Buy(b)
 	if err == nil {
 		t.Error("expected error, got nil")
+	}
+}
+
+func TestGetAllBooks(t *testing.T) {
+	t.Parallel()
+	catalog := map[int]bookstore.Book{
+		1: {ID: 1, Title: "The Go Programming Language"},
+		2: {ID: 2, Title: "Clean Code"},
+	}
+	want := []bookstore.Book{
+		{ID: 1, Title: "The Go Programming Language"},
+		{ID: 2, Title: "Clean Code"},
+	}
+	got := bookstore.GetAllBooks(catalog)
+	sort.Slice(got, func(i, j int) bool {
+		return got[i].ID < got[j].ID
+	})
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetBook(t *testing.T) {
+	t.Parallel()
+	catalog := map[int]bookstore.Book{
+		1: {ID: 1, Title: "The Go Programming Language"},
+		2: {ID: 2, Title: "The Go Programming Language2"},
+	}
+	want := bookstore.Book{Title: "The Go Programming Language", ID: 1}
+
+	got, err := bookstore.GetBook(catalog, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestPurchaseOfInvalidBookID(t *testing.T) {
+	t.Parallel()
+	catalog := map[int]bookstore.Book{}
+	_, err := bookstore.GetBook(catalog, 9999)
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+}
+
+func TestNetPriceCents(t *testing.T) {
+	t.Parallel()
+	book := bookstore.Book{
+		Title:           "book1",
+		Author:          "author1",
+		Copies:          1,
+		PriceCents:      4000,
+		DiscountPercent: 25,
+	}
+	want := 3000
+	got := book.NetPriceCents()
+	if want != got {
+		t.Errorf("want %d got %d", want, got)
 	}
 }
